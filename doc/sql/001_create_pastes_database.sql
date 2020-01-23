9f67694844fe0662e64685059f41169827b05692
Michal Hrusecky <Michal@Hrusecky.net>
2020-01-23 23:00:39 +0100

    Add example database schemas

---
+++ doc/sql/001_create_pastes_database.sql
@@ -0,0 +1,22 @@
+CREATE TABLE pastes (
+  `pid` char(16) NOT NULL,
+  `title` char(48) DEFAULT '',
+  `name` char(48) DEFAULT '',
+  `lang` char(48) DEFAULT 'text',
+  `private` boolean NOT NULL,
+  `created` int(10) NOT NULL,
+  `expire` int(10) NOT NULL DEFAULT 0,
+  `ip` char(50) DEFAULT '',
+  `login` char(160) NOT NULL DEFAULT '',
+  `replyto` char(16) DEFAULT '',
+  PRIMARY KEY (`pid`)
+);
+
+-- statement
+
+CREATE TABLE paste_datas (
+  `pid` char(16) NOT NULL,
+  `data` longtext NOT NULL,
+  PRIMARY KEY (`pid`),
+  FOREIGN KEY (`pid`) REFERENCES pastes(`pid`) ON DELETE CASCADE
+);
---
+++ doc/sql/999_example_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO pastes VALUES (
+    '123',
+    'Testing paste',
+    'Bond, James Bond',
+    'text',
+    False,
+    1,
+    0,
+    '',
+    '',
+    ''
+);
+
+INSERT INTO paste_datas VALUES (
+    '123',
+    'There is nothing here'
+);
