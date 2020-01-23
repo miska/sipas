d2ea77825253a00257b6232cffc36a7237c7917c
Michal Hrusecky <Michal@Hrusecky.net>
2020-01-24 00:26:38 +0100

    Use Nette form form pastes
    Visually step back, but should be better in long run.

--- app/Presenters/PastePresenter.php
+++ app/Presenters/PastePresenter.php
@@ -3,7 +3,7 @@
 namespace App\Presenters;
 
 use Nette;
-
+use Nette\Application\UI\Form;
 use GeSHi;
 
 class PastePresenter extends Nette\Application\UI\Presenter {
@@ -19,6 +19,39 @@ class PastePresenter extends Nette\Application\UI\Presenter {
         parent::startup();
         $this->geshi = new \GeSHi();
     }
+
+    protected function createComponentPasteForm(): Form {
+        $form = new Form;
+
+        $form->addText('name', 'Author:')
+             ->setHtmlAttribute('placeholder', 'Name yourself')
+		     ->addRule(Form::MAX_LENGTH, 'Your name has to be shorter than 48 characters!', 48);
+        $form->addText('title', 'Title:')
+             ->setHtmlAttribute('placeholder', 'Name your paste')
+		     ->addRule(Form::MAX_LENGTH, 'Your post title has to be shorter than 48 characters!', 48);
+        $form->addSelect('lang', 'Language:', $this->geshi->get_supported_languages(true))->setDefaultValue('text');
+        $form->addTextArea('paste', 'Paste:')
+             ->setRequired();
+        $form->addSelect('expire', 'Expire in', [
+                30 => "30 Minutes",
+                60 => "1 hour",
+                360 => "6 Hours",
+                720 => "12 Hours",
+                1440 => "1 Day",
+                10080 => "1 Week",
+                40320 => "1 Month",
+                151200 => "3 Monts",
+                604800 => "1 Year",
+                1209600 => "2 Years",
+                1814400 => "3 Years",
+                0 => "Never"
+               ])->setDefaultValue(10080);
+        $form->addCheckbox('private', 'Private paste');
+        $form->addSubmit('send', 'Paste');
+
+        return $form;
+    }
+
 
     public function renderShow(int $id): void {
         $paste = $this->database->table('pastes')->get($id);
